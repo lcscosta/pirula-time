5f96c20df6c84c9bd375ad9cfa500149d885f39a
lcscosta <lcscosta@disroot.org>
2024-10-13 22:41:12 -0300

    Added .env loading

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,34 @@
 
 use rocket::{get, launch, routes};
 use rocket_dyn_templates::Template;
+use dotenv::dotenv;
 use std::collections::HashMap;
+use std::env;
+
+fn verify_env_keys(key: &str) -> () {
+    match env::var(key) {
+        Ok(value) => println!("{}: {}", key, value),
+        Err(e) => eprintln!("Coldn't read Variable: {}", key)
+    }
+}
+
 
 #[launch]
 fn rocket() -> _ {
-   rocket::build()
-       .mount("/", routes![index])
-       .attach(Template::fairing())
+    dotenv().ok();
+
+    // Verify GOOGLE_API_KEY
+    verify_env_keys("GOOGLE_API_KEY");
+    // Verify CHANNEL_ID
+    verify_env_keys("CHANNEL_ID");
+    // Verify FILEPATH_DATABASE
+    verify_env_keys("FILEPATH_DATABASE");
+    // Verify ENVIRONMENT
+    verify_env_keys("ENVIRONMENT");
+
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
 
 #[get("/")]
