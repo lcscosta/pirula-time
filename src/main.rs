e601564f3990e3a8bcc03060296060c2f0a4bf42
Lucas da Costa <lcscosta@localhost.localdomain>
2024-10-13 22:21:49 -0300

    Added rocket.rs and rocket_dyn_templates start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use rocket::{get, launch, routes};
+use rocket_dyn_templates::Template;
+use std::collections::HashMap;
+
+#[launch]
+fn rocket() -> _ {
+   rocket::build()
+       .mount("/", routes![index])
+       .attach(Template::fairing())
 }
+
+#[get("/")]
+fn index() -> Template {
+    let context: HashMap<String, String> = HashMap::new();
+    Template::render("index", &context)
+}
+
